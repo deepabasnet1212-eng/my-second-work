c96426e63626cde5ab84d6e65744cad4605a9cf3
Deepa basnet <deepabasnet1212@gmail.com>
2025-12-29 08:51:42 +0545

    file created and commit

---
+++ c.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter an integer:");
+	scanf("%d",&x);
+	if (x==1)
+	{
+		printf("you are the one");
+	}
+else {
+	printf("you are not the one");
+	
+}
+	}
